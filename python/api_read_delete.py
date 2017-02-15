dcca968bf96fdd33331adab09725addfe29a04a8
Jeff P <jeffrey.perez@ost.orcid.org>
2017-02-15 13:56:06 -0600

    include 2.0 CRUD sequence + update clean up script

---
+++ python/api_read_delete.py
@@ -0,0 +1,69 @@
+import OrcidBaseTest
+import pyjavaproperties
+import json
+
+class ApiReadDelete(OrcidBaseTest.OrcidBaseTest):
+
+    def setUp(self):
+        p = pyjavaproperties.Properties()
+        p.load(open('test.properties'))
+        self.orcid_props   = p
+        self.orcid_id            = self.orcid_props['orcidId']
+        self.client_id           = self.orcid_props['memberClientId']
+        self.client_secret       = self.orcid_props['memberClientSecret']        
+        self.code                = self.orcid_props['api2PostUpdateCode']
+        self.token, self.refresh = self.orcid_exchange_auth_token(self.client_id,self.client_secret,self.code)
+
+    def test_get20_works(self):
+        self.assertIsNotNone(self.token, "No token generated")
+        curl_params = ['-H', 'Content-Type: application/orcid+json', '-H', 'Accept: application/json', '-H', 'Authorization: Bearer ' + str(self.token)]
+        response = self.orcid_curl("https://pub.qa.orcid.org/v2.0/%s/works" % self.orcid_id, curl_params)
+        json_response = json.loads(response)
+        group = json_response.get("group")
+        self.assertIsNotNone(group, "Group not found in JSON")
+        if (len(group) > 0):
+            for s in group:
+                putcode = s["works-summary"][0]["put-code"]
+                delete_results = self.remove_by_putcode(putcode, 'works')
+                self.assertEquals("", str(delete_results))
+
+    def test_get20_fundings(self):
+        self.assertIsNotNone(self.token, "No token generated")
+        curl_params = ['-H', 'Content-Type: application/orcid+json', '-H', 'Accept: application/json', '-H', 'Authorization: Bearer ' + str(self.token)]
+        response = self.orcid_curl("https://pub.qa.orcid.org/v2.0/%s/fundings" % self.orcid_id, curl_params)
+        json_response = json.loads(response)
+        group = json_response.get("group")
+        self.assertIsNotNone(group, "Group not found in JSON")
+        if (len(group) > 0):
+            for s in group:
+                putcode = s["funding-summary"][0]["put-code"]
+                delete_results = self.remove_by_putcode(putcode, 'funding')
+                self.assertEquals("", str(delete_results))
+
+    def test_get20_reviews(self):
+        self.assertIsNotNone(self.token, "No token generated")
+        curl_params = ['-H', 'Content-Type: application/orcid+json', '-H', 'Accept: application/json', '-H', 'Authorization: Bearer ' + str(self.token)]
+        response = self.orcid_curl("https://pub.qa.orcid.org/v2.0/%s/peer-reviews" % self.orcid_id, curl_params)
+        json_response = json.loads(response)
+        group = json_response.get("group")
+        self.assertIsNotNone(group, "Group not found in JSON")
+        if (len(group) > 0):
+            for s in group:
+                putcode = s["peer-review-summary"][0]["put-code"]
+                delete_results = self.remove_by_putcode(putcode, 'peer-review')
+                self.assertEquals("", str(delete_results))
+
+    def test_get20_educations(self):
+        self.assertIsNotNone(self.token, "No token generated")
+        curl_params = ['-H', 'Content-Type: application/orcid+json', '-H', 'Accept: application/json', '-H', 'Authorization: Bearer ' + str(self.token)]
+        response = self.orcid_curl("https://pub.qa.orcid.org/v2.0/%s/educations" % self.orcid_id, curl_params)
+        json_response = json.loads(response)
+        es = json_response.get("education-summary")
+        self.assertIsNotNone(es, "education-summary not found in JSON")
+        if (len(es) > 0):
+            for e in es:
+                putcode = e["put-code"]
+                delete_results = self.remove_by_putcode(putcode, 'education')
+                self.assertEquals("", str(delete_results))
+                
+        
--- python/test_member20_api_post_update.py
+++ python/test_member20_api_post_update.py
@@ -14,20 +14,64 @@ class Member20ApiPostUpdate(OrcidBaseTest.OrcidBaseTest):
         self.code          = self.orcid_props['api2PostUpdateCode']
         self.orcid_id      = self.orcid_props['orcidId']
         self.access,self.refresh = self.orcid_exchange_auth_token(self.client_id,self.client_secret,self.code)
-
-    def test_post_work(self):
-        # TEST 85 Post the ma test work 2
+        
+    def post_activity(self, activity_type = "work", xml_file = "ma2_work.xml"):
         self.assertIsNotNone(self.access,"Bearer not recovered: " + str(self.access))
-        curl_params = ['-i', '-L', '-H', 'Authorization: Bearer ' + str(self.access), '-H', 'Content-Type: application/orcid+xml', '-H', 'Accept: application/xml', '-d', '@' + self.xml_data_files_path + 'ma2_work.xml', '-X', 'POST']
-        response = self.orcid_curl("https://api.qa.orcid.org/v2.0/%s/work" % self.orcid_id, curl_params)
+        curl_params = ['-i', '-L', '-H', 'Authorization: Bearer ' + str(self.access), '-H', 'Content-Type: application/orcid+xml', '-H', 'Accept: application/xml', '-d', '@' + self.xml_data_files_path + xml_file, '-X', 'POST']
+        response = self.orcid_curl("https://api.qa.orcid.org/v2.0/%s/%s" % (self.orcid_id, activity_type) , curl_params)
+        return response
+    
+    def update_activity(self, putcode, updated_data, activity_type = "work"):
+        update_curl_params = ['-i', '-L', '-k', '-H', 'Authorization: Bearer ' + str(self.access), '-H', 'Content-Type: application/orcid+json', '-H', 'Accept: application/json', '-d', updated_data, '-X', 'PUT']
+        update_response = self.orcid_curl("https://api.qa.orcid.org/v2.0/%s/%s/%d" % (self.orcid_id, activity_type,int(putcode)), update_curl_params)
+        return update_response
+    
+    def delete_activity(self, putcode, activity_type = "work"):
+        delete_curl_params = ['-i', '-L', '-k', '-H', 'Authorization: Bearer ' + str(self.access), '-H', 'Content-Type: application/orcid+json', '-H', 'Accept: application/json', '-X', 'DELETE']
+        delete_response = self.orcid_curl("https://api.qa.orcid.org/v2.0/%s/%s/%d" % (self.orcid_id, activity_type, int(putcode)), delete_curl_params)
+        return delete_response
+    
+    def test_post_update_delete_work(self):
+        # TEST 85 Post the ma test work 2
+        response = self.post_activity("work", "ma2_work.xml")
         self.assertTrue("201 Created" in response, "Response missing \"Created\" tag: " + response)
         putcode = self.get_putcode_from_response(response)
         self.assertIsNotNone(putcode,"Not valid putcode returned: [%s]" % str(putcode))
         # TEST 88 Update the work with JSON
         self.assertFalse("" == putcode, "Empty putcode in url")
         updated_data = '{"put-code":' + str(putcode) + ',"title":{"title":"APITestTitleUpdated"},"type":"JOURNAL_ARTICLE","external-ids":{"external-id":[{"external-id-value":"1234","external-id-type":"doi","external-id-relationship":"SELF"}]}}'
-        update_curl_params = ['-i', '-L', '-k', '-H', 'Authorization: Bearer ' + str(self.access), '-H', 'Content-Type: application/orcid+json', '-H', 'Accept: application/json', '-d', updated_data, '-X', 'PUT']
-        update_response = self.orcid_curl("https://api.qa.orcid.org/v2.0/%s/work/%d" % (self.orcid_id,int(putcode)), update_curl_params)
+        update_response = self.update_activity(putcode, updated_data, "work")
         self.assertTrue("200 OK" in update_response, str(putcode) + " > Update Action Response: " + update_response + " with data [%s]" % updated_data)
         # TEST 90 Delete the work
+        delete_response = self.delete_activity(putcode, "work")
+        self.assertTrue("204 No Content" in delete_response, "Delete Action Response: " + delete_response + " using putcode [%s]" % str(putcode))
         
+    def test_post_update_delete_education(self):
+        # TEST 92 post education
+        response = self.post_activity("education", "ma2_edu.xml")
+        self.assertTrue("201 Created" in response, "Response missing \"Created\" tag: " + response)
+        putcode = self.get_putcode_from_response(response)
+        self.assertIsNotNone(putcode,"Not valid putcode returned: [%s]" % str(putcode))
+        # TEST delete
+        delete_response = self.delete_activity(putcode, "education")
+        self.assertTrue("204 No Content" in delete_response, "Delete Action Response: " + delete_response + " using putcode [%s]" % str(putcode))
+        
+    def test_post_update_delete_funding(self):
+        # TEST 92 post education
+        response = self.post_activity("funding", "ma2_fund2.xml")
+        self.assertTrue("201 Created" in response, "Response missing \"Created\" tag: " + response)
+        putcode = self.get_putcode_from_response(response)
+        self.assertIsNotNone(putcode,"Not valid putcode returned: [%s]" % str(putcode))
+        # TEST delete
+        delete_response = self.delete_activity(putcode, "funding")
+        self.assertTrue("204 No Content" in delete_response, "Delete Action Response: " + delete_response + " using putcode [%s]" % str(putcode))
+        
+    def test_post_update_delete_peerreview(self):
+        # TEST 92 post education
+        response = self.post_activity("peer-review", "ma2_peer2.xml")
+        self.assertTrue("201 Created" in response, "Response missing \"Created\" tag: " + response)
+        putcode = self.get_putcode_from_response(response)
+        self.assertIsNotNone(putcode,"Not valid putcode returned: [%s]" % str(putcode))
+        # TEST delete
+        delete_response = self.delete_activity(putcode, "peer-review")
+        self.assertTrue("204 No Content" in delete_response, "Delete Action Response: " + delete_response + " using putcode [%s]" % str(putcode))
