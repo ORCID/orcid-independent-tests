4a0ba4d31332c983207088d0e2a38608b21d6377
Jeff P <jeffrey.perez@ost.orcid.org>
2017-02-27 14:13:07 -0600

    update jenkins with latest G codes and assert not empty token

--- orcid/test_member12_api_post_update.py
+++ orcid/test_member12_api_post_update.py
@@ -17,36 +17,43 @@ class Member12ApiPostUpdate(OrcidBaseTest.OrcidBaseTest):
         self.access,self.refresh = self.orcid_exchange_auth_token(self.client_id,self.client_secret,self.code)
 
     def test_post_work(self):
+        self.assertIsNotNone(self.access, "Not token generated: [%s]" % str(self.access))
         curl_params = ['-L', '-i', '-k', '-H', 'Authorization: Bearer ' + self.access, '-H', 'Content-Type: application/orcid+xml', '-H', 'Accept: application/xml', '-d', '@' + self.xml_data_files_path + 'ma_work.xml', '-X', 'POST']
         response = self.orcid_curl("http://api.qa.orcid.org/v1.2/%s/orcid-works" % self.orcid_id, curl_params)
         self.assertTrue("201 Created" in response, "response: " + response)
 
     def test_update_work(self):
+        self.assertIsNotNone(self.access, "Not token generated: [%s]" % str(self.access))
         curl_params = ['-L', '-i', '-k', '-H', 'Authorization: Bearer ' + self.access, '-H', 'Content-Type: application/orcid+xml', '-H', 'Accept: application/xml', '-d', '@' + self.xml_data_files_path + 'ma_work2.xml', '-X', 'PUT']
         response = self.orcid_curl("http://api.qa.orcid.org/v1.2/%s/orcid-works" % self.orcid_id, curl_params)
         self.assertTrue("200 OK" in response, "response: " + response)
 
     def test_post_funding(self):
+        self.assertIsNotNone(self.access, "Not token generated: [%s]" % str(self.access))
         curl_params = ['-L', '-i', '-k', '-H', 'Authorization: Bearer ' + self.access, '-H', 'Content-Type: application/orcid+xml', '-H', 'Accept: application/xml', '-d', '@' + self.xml_data_files_path + 'ma_fund.xml', '-X', 'POST']
         response = self.orcid_curl("http://api.qa.orcid.org/v1.2/%s/funding" % self.orcid_id, curl_params)
         self.assertTrue("201 Created" in response, "response: " + response)
 
     def test_update_funding(self):
+        self.assertIsNotNone(self.access, "Not token generated: [%s]" % str(self.access))
         curl_params = ['-L', '-i', '-k', '-H', 'Authorization: Bearer ' + self.access, '-H', 'Content-Type: application/orcid+xml', '-H', 'Accept: application/xml', '-d', '@' + self.xml_data_files_path + 'ma_fund2.xml', '-X', 'PUT']
         response = self.orcid_curl("http://api.qa.orcid.org/v1.2/%s/funding" % self.orcid_id, curl_params)
         self.assertTrue("200 OK" in response, "response: " + response)
 
     def test_post_education(self):
+        self.assertIsNotNone(self.access, "Not token generated: [%s]" % str(self.access))
         curl_params = ['-L', '-i', '-k', '-H', 'Authorization: Bearer ' + self.access, '-H', 'Content-Type: application/orcid+xml', '-H', 'Accept: application/xml', '-d', '@' + self.xml_data_files_path + 'ma_edu.xml', '-X', 'POST']
         response = self.orcid_curl("http://api.qa.orcid.org/v1.2/%s/affiliations" % self.orcid_id, curl_params)
         self.assertTrue("201 Created" in response, "response: " + response)
 
     def test_update_education(self):
+        self.assertIsNotNone(self.access, "Not token generated: [%s]" % str(self.access))
         curl_params = ['-L', '-i', '-k', '-H', 'Authorization: Bearer ' + self.access, '-H', 'Content-Type: application/orcid+xml', '-H', 'Accept: application/xml', '-d', '@' + self.xml_data_files_path + 'ma_edu2.xml', '-X', 'PUT']
         response = self.orcid_curl("http://api.qa.orcid.org/v1.2/%s/affiliations" % self.orcid_id, curl_params)
         self.assertTrue("200 OK" in response, "response: " + response)
 
     def test_post_bio(self):
+        self.assertIsNotNone(self.access, "Not token generated: [%s]" % str(self.access))
         curl_params = ['-L', '-i', '-k', '-H', 'Authorization: Bearer ' + self.access, '-H', 'Content-Type: application/orcid+xml', '-H', 'Accept: application/xml', '-d', '@' + self.xml_data_files_path + 'ma_bio.xml', '-X', 'PUT']
         response = self.orcid_curl("http://api.qa.orcid.org/v1.2/%s/orcid-bio" % self.orcid_id, curl_params)
         self.assertTrue("200 OK" in response, "response: " + response)
