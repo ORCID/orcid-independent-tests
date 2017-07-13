d5c2670388305abade095e02869d580e08465328
Catalina Oyler <c.oyler@orcid.org>
2017-07-13 09:40:11 -0700

    fix name of ex-id test

--- orcid/api_read_delete.py
+++ orcid/api_read_delete.py
@@ -78,7 +78,7 @@ class ApiReadDelete(OrcidBaseTest.OrcidBaseTest):
                 delete_results = self.remove_by_putcode(putcode, 'keyword')
                 #self.assertEquals("", str(delete_results))
                 
-    def test_remove_external-ids(self):
+    def test_remove_externalids(self):
         self.assertIsNotNone(self.token, "No token generated")
         curl_params = ['-H', 'Content-Type: application/orcid+json', '-H', 'Accept: application/json', '-H', 'Authorization: Bearer ' + str(self.token)]
         response = self.orcid_curl("https://api." + properties.test_server + "/v2.0/%s/external-identifiers" % self.orcid_id, curl_params)
