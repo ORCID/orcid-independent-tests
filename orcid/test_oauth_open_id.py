e4c9e58e56eddc8860f33387b5fa060574b763d0
Jeff P <jeff@siccr.com>
2018-05-28 17:10:24 -0600

    include base openid independent tests

---
+++ orcid/test_oauth_open_id.py
@@ -0,0 +1,37 @@
+import OrcidBaseTest
+import properties
+
+class OauthOpenId(OrcidBaseTest.OrcidBaseTest):
+
+    def setUp(self):
+        self.client_id = 'APP-96ONQ4RSQY8KWXVZ'
+
+    def test_grant_with_implicit_request(self):
+        self.assertTrue(1)
+
+    def test_generate_read_limited(self):
+        self.assertTrue(1)
+
+    def test_not_exchanged_expires(self):
+        self.assertTrue(1)
+
+    def test_call_user_info(self):
+        self.assertTrue(1)
+
+    def test_generate_exchange(self):
+        self.assertTrue(1)
+
+    def test_generate_revoke(self):
+        self.assertTrue(1)
+
+    def test_generate_auth_code_with_openid_scope(self):
+        self.assertTrue(1)
+
+    def test_all_endpoints(self):
+        self.assertTrue(1)
+
+
+
+
+
+
