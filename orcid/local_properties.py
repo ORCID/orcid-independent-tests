19b5b92db7e3ba3188744fa7df962b2c05188b1e
andrej romanov <50377758+auumgn@users.noreply.github.com>
2019-11-19 16:59:48 +0200

    user obo testing

---
+++ orcid/local_properties.py
@@ -0,0 +1,11 @@
+type="jenkins"
+
+orcid_id = "0000-0002-7361-1027"
+access = "b644e5fe-006d-47dc-b3b3-334f06ddfac3"
+step_1_access = "06702255-c514-4ecc-a225-9c48446a9173"
+group_access = "6b00136d-b878-4b3b-991f-4ea8ecb34465"
+
+access_member = "299e0132-623d-4024-9b47-6c9a0e042b39"
+orcid_id_member = "0000-0001-6009-1985"
+notify_token = "eafafe49-b5bf-41db-9fb5-ad3a6cba575b"
+webhook = "af36161d-0971-4ac6-b860-5bb3f7cdef664"
--- orcid/test_member30_api_post_update.py
+++ orcid/test_member30_api_post_update.py
@@ -1,22 +1,31 @@
 import OrcidBaseTest
 import properties
+import local_properties
 
 class Member20ApiPostUpdate(OrcidBaseTest.OrcidBaseTest):
 
     def setUp(self):
+      self.version = "/v3.0/"
+      if local_properties.type == "jenkins":
         self.client_id     = properties.memberClientId
         self.client_secret = properties.memberClientSecret
         self.notify_token  = properties.notifyToken
         self.webhook_access= self.orcid_generate_token(self.client_id, self.client_secret, "/webhook")
-        self.version	   = "/v3.0/"
-        #Comment out below when testing locally
         self.orcid_id      = properties.orcidId
         self.scope               = "/read-limited%20/activities/update%20/person/update"
         self.code                = self.generate_auth_code(self.client_id,self.scope, "api2PostUpdateCode")
         self.access,self.refresh = self.orcid_exchange_auth_token(self.client_id,self.client_secret,self.code)
-        #Use below when testing locally
-		#self.access = ""
-		#self.orcid_id = ""
+      else:
+        self.orcid_id = local_properties.orcid_id_member
+        self.access = local_properties.access_member
+        self.notify_token = local_properties.notify_token
+        self.webhook_access = local_properties.webhook
+        self.user_obo_id = "APP-52PDPI669AHFVT3V"
+        self.user_obo_secret = "8e242970-5d2a-4b47-b7d3-c88165a10bfb"
+        self.user_obo_scope = "openid%20/read-limited%20/activities/update%20/person/update"
+        self.user_obo_code = self.generate_auth_code(self.user_obo_id, self.user_obo_scope, "api2PostUpdateCode")
+        self.user_obo_access, self.user_obo_refresh = self.orcid_exchange_auth_token(self.user_obo_id, self.user_obo_secret, self.user_obo_code)
+
     
     def test_post_update_work(self):
         #Post a work using 3.0 to the record created for testing today
@@ -43,17 +52,17 @@ class Member20ApiPostUpdate(OrcidBaseTest.OrcidBaseTest):
         self.assertTrue("201 Created" in response, "Response missing \"Created\" tag: " + response)
         
     def test_post_funding(self):
-    	# Post a funding itme using 3.0 to the record created for testing today
+        # Post a funding itme using 3.0 to the record created for testing today
         response = self.post_activity(self.version, "funding", "ma30_fund.xml")
         self.assertTrue("201 Created" in response, "Response missing \"Created\" tag: " + response)
         
     def test_post_peerreview(self):
-    	# Post a peer-review using 3.0 to the record created for testing today
+        # Post a peer-review using 3.0 to the record created for testing today
         response = self.post_activity(self.version, "peer-review", "ma30_peer.xml")
         self.assertTrue("201 Created" in response, "Response missing \"Created\" tag: " + response)
         
     def test_post_researchresource(self):
-    	# Post a research-resource itme using 3.0 to the record created for testing today
+        # Post a research-resource itme using 3.0 to the record created for testing today
         response = self.post_activity(self.version, "research-resource", "ma30_rr.xml")
         self.assertTrue("201 Created" in response, "Response missing \"Created\" tag: " + response)
         
@@ -70,17 +79,17 @@ class Member20ApiPostUpdate(OrcidBaseTest.OrcidBaseTest):
         self.assertTrue("200 OK" in update_response, str(putcode) + " > Update Action Response: " + update_response + " with data [%s]" % updated_data)
              
     def test_post_othername(self):
-    	# Post an other name using 3.0 to the record created for testing today
+        # Post an other name using 3.0 to the record created for testing today
         response = self.post_activity(self.version, "other-names", "ma30_othername.xml")
         self.assertTrue("201 Created" in response, "Response missing \"Created\" tag: " + response) 
         
     def test_post_country(self):
-    	# Post a country using 3.0 to the record created for testing today
+        # Post a country using 3.0 to the record created for testing today
         response = self.post_activity(self.version, "address", "ma30_country.xml")
         self.assertTrue("201 Created" in response, "Response missing \"Created\" tag: " + response) 
         
     def test_post_website(self):
-    	# Post a website using 3.0 to the record created for testing today
+        # Post a website using 3.0 to the record created for testing today
         response = self.post_activity(self.version, "researcher-urls", "ma30_website.xml")
         self.assertTrue("201 Created" in response, "Response missing \"Created\" tag: " + response) 
         
@@ -106,6 +115,17 @@ class Member20ApiPostUpdate(OrcidBaseTest.OrcidBaseTest):
         # Post a bulk works item using 3.0 to the record created for testing today
         response = self.post_activity(self.version, "works", "ma30_bulkworks.xml")
         self.assertTrue("200 OK" in response, "Response missing \"Created\" tag: " + response)
-	self.assertFalse("400 Bad Request" in response, "badly formed XML error in response " + response)
-	self.assertFalse("409 Conflict" in response, "Already posted this work error in response " + response)
-        
+        self.assertFalse("400 Bad Request" in response, "badly formed XML error in response " + response)
+        self.assertFalse("409 Conflict" in response, "Already posted this work error in response " + response)
+
+   # def test_user_obo(self):
+    def test_post_update_work(self):
+        #Post a work using 3.0 to the record created for testing today
+        response = self.post_user_obo(self.version, "work", "ma30_work.xml")
+        self.assertTrue("201 Created" in response, "Response missing \"Created\" tag: " + response)
+
+    def post_user_obo(self, version, activity_type = "work", xml_file = "ma2_work.xml"):
+        self.assertIsNotNone(self.user_obo_access,"Bearer not recovered: " + str(self.user_obo_access))
+        curl_params = ['-i', '-L', '-H', 'Authorization: Bearer ' + str(self.user_obo_access), '-H', 'Content-Type: application/orcid+xml', '-H', 'Accept: application/xml', '-d', '@' + self.xml_data_files_path + xml_file, '-X', 'POST']
+        response = self.orcid_curl("https://api." + properties.test_server + version + "%s/%s" % (self.orcid_id, activity_type) , curl_params)
+        return response
