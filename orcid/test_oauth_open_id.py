741d514ced165136ef618a86c05ffb2de967bd63
andrej romanov <50377758+auumgn@users.noreply.github.com>
2019-12-10 00:55:38 +0200

    Update test_oauth_open_id.py

--- orcid/test_oauth_open_id.py
+++ orcid/test_oauth_open_id.py
@@ -1,47 +1,45 @@
 import OrcidBaseTest
 from OrcidBrowser import OrcidBrowser
 import properties
+import json
 
 class OauthOpenId(OrcidBaseTest.OrcidBaseTest):
 
     def setUp(self):
         self.firefox = OrcidBrowser()
-        self.public_record_id    = properties.staticId
-        self.public_record_token = "c9974dc3-451b-420f-ae6e-b76d7009062e"
-        self.limited_record_token = "2fe47c3c-aae6-4a80-981b-fc221a067abe"
-        self.client_id = properties.OpenClientId
-        self.client_secret = properties.OpenClientSecret
-        self.scope = "openid"
-        self.wrong_scope = "/read-limited%20/activities/update%20/person/update"
-
         self.member_obo_id = properties.OBOMemberClientId
         self.member_obo_secret = properties.OBOMemberClientSecret
         self.client_id     = properties.memberClientId
         self.client_secret = properties.memberClientSecret
+        self.openid_scope = "openid"
         self.member_obo_scope = "openid%20/read-limited%20/activities/update%20/person/update"
         self.member_obo_code = self.generate_auth_code(self.member_obo_id, self.member_obo_scope, "api2PostUpdateCode")
         self.member_obo_access, self.member_obo_refresh, self.member_obo_id_token = self.orcid_exchange_auth_token(self.member_obo_id,self.member_obo_secret,self.member_obo_code)
+        self.member_obo_code = self.generate_auth_code(self.member_obo_id, self.member_obo_scope, "api2PostUpdateCode")
+        self.member_obo_access, self.member_obo_refresh, self.member_obo_id_token = self.orcid_exchange_auth_token(self.member_obo_id, self.member_obo_secret, self.member_obo_code)
 
     def test_existing_token_flow(self):
         id_token = self.get_id_token()
         print "id_token: " + id_token
-        newaccess = self.get_obo_token()
+        newaccess = self.get_obo_token(id_token)
         print "new access token: " + newaccess
 
     def get_id_token(self):
         self.assertIsNotNone(self.member_obo_access,"Bearer not recovered: " + str(self.member_obo_access))
-        curl_params = ['-i', '-L', '-H', "Accept: application/json", '--data' 'client_id=' + self.member_obo_id + '&client_secret=' + self.member_obo_secret + '&subject_token=' + self.member_obo_access +
+        curl_params = ['-i', '-L', '-H', "Accept: application/json", '--data', 'client_id=' + self.member_obo_id + '&client_secret=' + self.member_obo_secret + '&subject_token=' + self.member_obo_access +
         '&grant_type=urn:ietf:params:oauth:grant-type:token-exchange&subject_token_type=urn:ietf:params:oauth:token-type:access_token&requested_token_type=urn:ietf:params:oauth:token-type:id_token']
         response = self.orcid_curl("https://" + properties.test_server + "/oauth/token", curl_params)
-        return response
+        json_response = json.loads(response)
+        return json_response['id_token']
 
-    def get_obo_token(self):
-        curl_params = ['-i', '-L', '-H', "Accept: application/json", '--data' 'client_id=' + self.client_id + '&client_secret=' + self.client_secret +
-                     '&grant_type=urn:ietf:params:oauth:grant-type:token-exchange&subject_token=' +
+    def get_obo_token(self, id_token):
+        curl_params = ['-i', '-L', '-H', "Accept: application/json", '--data', 'client_id=' + self.client_id + '&client_secret=' + self.client_secret +
+                     '&grant_type=urn:ietf:params:oauth:grant-type:token-exchange&subject_token=' + id_token +
                      '&subject_token_type=urn:ietf:params:oauth:token-type:id_token&requested_token_type=urn:ietf:params:oauth:token-type:access_token']
 
         response = self.orcid_curl("https://" + properties.test_server + "/oauth/token", curl_params)
-        return response
+        json_response = json.loads(response)
+        return json_response['access_token']
 '''
     def test_oauth_token(self):
         code = self.generate_auth_code(self.client_id, self.scope, "open")
