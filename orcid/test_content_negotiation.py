e1fbf0a5b6f80dfc262f92a1ede06e47bb82dabd
andrej romanov <50377758+auumgn@users.noreply.github.com>
2021-08-27 18:17:20 +0300

    allow different test servers

--- orcid/test_content_negotiation.py
+++ orcid/test_content_negotiation.py
@@ -26,20 +26,20 @@ class ContentNegotiation(OrcidBaseTest.OrcidBaseTest):
     def test_010_xml_call(self):
         response = self.read_record("application/xml")
         self.assertTrue("HTTP/1.1 200" in response, "Invalid response code:\n" + response)
-        self.assertTrue("Location: https://pub.qa.orcid.org/v3.0/0000-0002-7361-1027" in response, "Location missing:\n" + response)
+        self.assertTrue("Location: https://pub.%s/v3.0/0000-0002-7361-1027" % (self.test_server) in response, "Location missing:\n" + response)
         self.assertTrue("Content-Type: application/xml" in response, "Incorrect Content-Type:\n" + response)
-        self.assertTrue("<common:uri>https://qa.orcid.org/0000-0002-7361-1027</common:uri>" in response, 'Unable to find "<common:uri>https://qa.orcid.org/0000-0002-7361-1027</common:uri>"" in the response:\n' + response)
+        self.assertTrue("<common:uri>https://%s/0000-0002-7361-1027</common:uri>" % (self.test_server) in response, 'Unable to find "<common:uri>https://%s/0000-0002-7361-1027</common:uri>"" in the response:\n' % (self.test_server) + response)
 
     def test_011_json_call(self):
         response = self.read_record("application/orcid+json")
         self.assertTrue("HTTP/1.1 200" in response, "Invalid response code:\n" + response)
-        self.assertTrue("Location: https://pub.qa.orcid.org/v3.0/0000-0002-7361-1027" in response, "Location missing:\n" + response)
+        self.assertTrue("Location: https://pub.%s/v3.0/0000-0002-7361-1027" % (self.test_server) in response, "Location missing:\n" + response)
         self.assertTrue("Content-Type: application/orcid+json" in response, "Incorrect Content-Type:\n" + response)
-        self.assertTrue('"uri" : "https://qa.orcid.org/0000-0002-7361-1027"' in response, 'Unable to find "uri" : "https://qa.orcid.org/0000-0002-7361-1027", in the response:\n' + response)
+        self.assertTrue('"uri" : "https://%s/0000-0002-7361-1027"' % (self.test_server) in response, 'Unable to find "uri" : "https://%s/0000-0002-7361-1027", in the response:\n' % (self.test_server) + response)
 
     def test_011_turtle_call(self):
         response = self.read_record("text/turtle")
         self.assertTrue("HTTP/1.1 200" in response, "Invalid response code:\n" + response)
-        self.assertTrue("Location: https://pub.qa.orcid.org/experimental_rdf_v1/0000-0002-7361-1027" in response, "Location missing:\n" + response)
+        self.assertTrue("Location: https://pub.%s/experimental_rdf_v1/0000-0002-7361-1027" % (self.test_server) in response, "Location missing:\n" + response)
         self.assertTrue("Content-Type: text/turtle" in response, "Incorrect Content-Type:\n" + response)
-        self.assertTrue('<https://qa.orcid.org/0000-0002-7361-1027#orcid-id>' in response, 'Unable to find "<https://qa.orcid.org/0000-0002-7361-1027#orcid-id>", in the response:\n' + response)
+        self.assertTrue('<https://%s/0000-0002-7361-1027#orcid-id>' % (self.test_server) in response, 'Unable to find "<https://%s/0000-0002-7361-1027#orcid-id>", in the response:\n' % (self.test_server) + response)
