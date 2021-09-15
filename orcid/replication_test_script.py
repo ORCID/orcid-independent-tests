00811ed89bd7d813cedd8266cc9c6ffaccaea02f
andrej romanov <50377758+auumgn@users.noreply.github.com>
2021-09-15 19:29:40 +0300

    update replication script

--- orcid/replication_test_script.py
+++ orcid/replication_test_script.py
@@ -1,9 +1,12 @@
 import subprocess
+import re
+import os
 import time
 import json
 import unittest
 import random
 import threading
+import logging
 import xml.dom.minidom as md
 
 class Replication(unittest.TestCase):
@@ -11,6 +14,7 @@ class Replication(unittest.TestCase):
         self.orcid_id = "0000-0001-6009-1985"
         self.access_token = "715ee62c-573e-4cdd-beff-6baae3890cce"     
         self.source = "Automated Test Helper"
+        self.formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
         self.curl_params_get = ['-L', '-i', '-k', '-H', 'Authorization: Bearer %s' % self.access_token, '-H', 'Content-Length: 0', '-H', 'Accept: application/json', '-k', '-X', 'GET']
         '''if properties.type == "actions":
           self.test_server = properties.test_server
@@ -18,51 +22,75 @@ class Replication(unittest.TestCase):
         else:
           self.test_server = local_properties.test_server
           self.access_token = properties.staticAccess'''
-    def get_record(self, token):
-        curl_params = ['-L', '-i', '-k', '-H', 'Authorization: Bearer %s' % token, '-H', 'Content-Length: 0', '-H', 'Accept: application/json', '-k', '-X', 'GET']
-        
             
     def cleanup(self):
-        response = self.orcid_curl("https://api.qa.orcid.org/v3.0/%s/record" % self.orcid_id, self.curl_params_get)
-        print ("{" + response.partition('{')[2])
+        response = self.orcid_curl("https://api.qa.orcid.org/v3.0/%s/record" % self.orcid_id, self.curl_params_get, None)
         record = json.loads("{" + response.partition('{')[2])['activities-summary']
         try:
-            for x in record['educations']['affiliation-group']:
-                for y in x['summaries']:
-                    if y['education-summary']['source']['source-name']['value'] == self.source:
-                        self.delete_activity(y['education-summary']['put-code'], "education")
-                        print("edu deleted")
+            print("Education: ", end="", flush=True)
+            if record['educations']['affiliation-group']:
+                for x in record['educations']['affiliation-group']:
+                    for y in x['summaries']:                    
+                        if y['education-summary']['source']['source-name']['value'] == self.source:
+                            self.delete_activity("education", y['education-summary']['put-code'], None)                        
+                            print(y['education-summary']['put-code'], "", end="", flush=True)                    
+            else:
+                print("No activities found", end="", flush=True)
+                
+            print("\nFundings: ", end="", flush=True)
+            if record['fundings']['group']:
+                for x in record['fundings']['group']:
+                    for y in x['funding-summary']:
+                        if y['source']['source-name']['value'] == self.source:
+                            self.delete_activity("funding", y['put-code'], None)
+                            print(y['put-code'],  "", end="", flush=True)
+            else:
+                print("No activities found", end="", flush=True)
+            
+            print("\nWorks: ", end="", flush=True)
+            if record['works']['group']:
+                for x in record['works']['group']:
+                    for y in x['work-summary']:
+                        if y['source']['source-name']['value'] == self.source:
+                            self.delete_activity("work", y['put-code'], None)
+                            print (y['put-code'], "", end="", flush=True)
+            else:
+                print("No activities found", end="", flush=True)
 
-            for x in record['fundings']['group']:
-                for y in x['funding-summary']:
-                    if y['source']['source-name']['value'] == self.source:
-                        self.delete_activity(y['put-code'], "funding")
-                        print("funding deleted")
-
-            for x in record['works']['group']:
-                for y in x['work-summary']:
-                    if y['source']['source-name']['value'] == self.source:
-                        self.delete_activity(y['put-code'], "work")
-                        if y['source']['assertion-origin-name']['value']:
-                          if y['source']['assertion-origin-name']['value'] == self.source:
-                            self.delete_activity(y['put-code'], "work")
-                            print("work deleted")
-
-            for x in record['peer-reviews']['group']:
-                for y in x['peer-review-group']:
-                    if y['peer-review-summary'][0]['source']['source-name']['value'] == self.source:
-                        self.delete_activity(y['peer-review-summary'][0]['put-code'], "peer-review")
-                        print("pr deleted")
+            print("\nPeer reviews: ", end="", flush=True)
+            if record['peer-reviews']['group']:
+                for x in record['peer-reviews']['group']:
+                    for y in x['peer-review-group']:
+                        if y['peer-review-summary'][0]['source']['source-name']['value'] == self.source:
+                            self.delete_activity("peer-review", y['peer-review-summary'][0]['put-code'], None)
+                            print(y['peer-review-summary'][0]['put-code'], "", end="", flush=True)
+            else:
+                print("No activities found")
+                
         except TypeError as e:
-            raise (e)                        
-    
-    def orcid_curl(self, url, curl_opts, rec=False):
+            raise (e)       
+            
+    def logger_setup(self, name, log_file, level=logging.INFO):
+        if not os.path.exists("./logs"):
+            os.mkdir("./logs")
+   
+        handler = logging.FileHandler(log_file)                  
+        handler.setFormatter(self.formatter)
+        
+        logger = logging.getLogger(name)
+        logger.setLevel(level)
+        logger.addHandler(handler)
+
+        return logger 
+        
+    def orcid_curl(self, url, curl_opts, logger):
         curl_call = ["curl"] + curl_opts + [url]
         while True:
             try:
-                p = subprocess.Popen(curl_call, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                #print(subprocess.list2cmdline(curl_call))
+                p = subprocess.Popen(curl_call, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)                
                 output,err = p.communicate()
+                if logger is not None:                    
+                    logger.info(subprocess.list2cmdline(curl_call))
                 return output.decode()
             except Exception as e:
                 raise Exception(e)
@@ -77,86 +105,87 @@ class Replication(unittest.TestCase):
         return putcode
             
             
-    def update_xml(self, xml_file, tag, value, put=False):
-        file = md.parse( "../post_files/" + xml_file) 
-        file.getElementsByTagName(tag)[0].firstChild.nodeValue = value
-        root = file.documentElement
+    def update_xml(self, xml, tag, value, put=False):
+        xml = re.sub('put-code=".*?"', '', xml)
+        xml = re.sub("<common:external-id-value>(.*?)</common:external-id-value>", "<common:external-id-value>%s</common:external-id-value>" % value, xml)
         if put:
-            root.setAttribute("put-code", value)
-        else:
-            if (root.hasAttribute("put-code")):
-                root.removeAttribute("put-code")
-        with open("../post_files/" + xml_file, "w") as fs: 
-            fs.write(file.toxml())
-            fs.close()     
+            xml = re.sub('xmlns:common="http://www.orcid.org/ns/common"', 'put-code="%s" xmlns:common="http://www.orcid.org/ns/common"' % value, xml)
+        return xml
         
-    def update_activity(self, endpoint, putcode, xml_file):
-        self.update_xml(xml_file, "common:external-id-value", putcode, True)
+    def update_activity(self, endpoint, putcode, xml, logger):
+        xml = self.update_xml(xml, "common:external-id-value", putcode, True)
         curl_params = ['-i', '-L', '-H', 'Authorization: Bearer ' + self.access_token, '-H',
-                       'Content-Type: application/orcid+xml', '-H', 'Accept: application/xml', '-d', '@' + "../post_files/" + xml_file, '-X', 'PUT']
-        response = self.orcid_curl("https://api.qa.orcid.org/v3.0/%s/%s/%s" % (self.orcid_id, endpoint, putcode), curl_params)
+                       'Content-Type: application/orcid+xml', '-H', 'Accept: application/xml', '-d', xml, '-X', 'PUT']
+        response = self.orcid_curl("https://api.qa.orcid.org/v3.0/%s/%s/%s" % (self.orcid_id, endpoint, putcode), curl_params, logger)
+        self.assertTrue("<common:external-id-value>%s</common:external-id-value>" % putcode in response, "Putcode %s missing from response: \n" % putcode + response)
     
-    def post_activity(self, endpoint, xml_file, count):
-        self.update_xml(xml_file, "common:external-id-value", count)
+    def post_activity(self, endpoint, xml, count, logger):
+        xml = self.update_xml(xml, "common:external-id-value", count)
         curl_params = ['-i', '-L', '-H', 'Authorization: Bearer ' + self.access_token, '-H',
-                       'Content-Type: application/orcid+xml', '-H', 'Accept: application/xml', '-d', '@' + "../post_files/" + xml_file, '-X', 'POST']
-        response = self.orcid_curl("https://api.qa.orcid.org/v3.0/%s/%s" % (self.orcid_id, endpoint), curl_params)
+                       'Content-Type: application/orcid+xml', '-H', 'Accept: application/xml', '-d', xml, '-X', 'POST']
+        response = self.orcid_curl("https://api.qa.orcid.org/v3.0/%s/%s" % (self.orcid_id, endpoint), curl_params, logger)
+        self.assertTrue("HTTP/1.1 201" in response, "201 code missing from response: \n" + response)
         putcode = self.getputcode(response)        
         return putcode
     
-    def delete_activity(self, endpoint, putcode):
+    def delete_activity(self, endpoint, putcode, logger):
         curl_params = ['-L', '-i', '-k', '-H', 'Authorization: Bearer %s' % self.access_token, '-H', 'Content-Length: 0', '-H', 'Accept: application/orcid+xml', '-k', '-X', 'DELETE']
-        response = self.orcid_curl("https://api.qa.orcid.org/v3.0/%s/%s/%s" % (self.orcid_id, endpoint, putcode), curl_params)
+        response = self.orcid_curl("https://api.qa.orcid.org/v3.0/%s/%s/%s" % (self.orcid_id, endpoint, putcode), curl_params, logger)
+        self.assertTrue("HTTP/1.1 204" in response, "204 code missing from response: \n" + response)
 
-    def confirmAddedWorks(self, endpoint, putcode):
-        response = self.orcid_curl("https://api.qa.orcid.org/v3.0/%s/%s/%s" % (self.orcid_id, endpoint, putcode), self.curl_params_get)
+    def confirmAddedWorks(self, endpoint, putcode, logger):
+        response = self.orcid_curl("https://api.qa.orcid.org/v3.0/%s/%s/%s" % (self.orcid_id, endpoint, putcode), self.curl_params_get, logger)
         self.assertTrue("HTTP/1.1 200" in response, "200 code missing from response: \n" + response)
         
-    def confirmUpdatedWorks(self, endpoint, putcode):
-        response = self.orcid_curl("https://api.qa.orcid.org/v3.0/%s/%s/%s" % (self.orcid_id, endpoint, putcode), self.curl_params_get)
+    def confirmUpdatedWorks(self, endpoint, putcode, logger):
+        response = self.orcid_curl("https://api.qa.orcid.org/v3.0/%s/%s/%s" % (self.orcid_id, endpoint, putcode), self.curl_params_get, logger)
         self.assertTrue('"external-id-value":"%s"' % putcode in response, "Putcode %s missing from external identifier in response: \n" % putcode + response)
         
-    def confirmRemovedWorks(self, endpoint, putcode):
-        response = self.orcid_curl("https://api.qa.orcid.org/v3.0/%s/%s/%s" % (self.orcid_id, endpoint, putcode), self.curl_params_get)
+    def confirmRemovedWorks(self, endpoint, putcode, logger):
+        response = self.orcid_curl("https://api.qa.orcid.org/v3.0/%s/%s/%s" % (self.orcid_id, endpoint, putcode), self.curl_params_get, logger)
         self.assertTrue("HTTP/1.1 404" in response, "404 code missing from response: \n" + response)
     
-    def replicationTest(self, thread_name, endpoint, xml_file):
+    def replicationTest(self, thread_name, endpoint, xml_file):   
+        logger = self.logger_setup(endpoint, "./logs/Replication_test_%s.log" % thread_name.lower())
         count = 0
         putcodes = []
         updatedPutcodes = []
         deletedPutcodes = []
+        with open("../post_files/" + xml_file, 'r') as file:
+            xml = file.read() 
+        print("%s: Adding %ss" % (thread_name, endpoint))
         while count < 10:
-            putcode = self.post_activity(endpoint, xml_file, count)
+            putcode = self.post_activity(endpoint, xml, count, logger)
             putcodes.append(putcode)       
-            print("%s: %s %s added" % (thread_name, endpoint, putcode))
+            logger.info("%s: %s %s added" % (thread_name, endpoint.capitalize(), putcode))
             count += 1
             
-        print (thread_name, ": ", putcodes)
+        print("%s: Works added - %s" % (thread_name, putcodes))
         for putcode in putcodes:
-            self.confirmAddedWorks(endpoint, putcode)
-        print("%s: Confirmation - %ss added" % (thread_name, endpoint))
-        
+            self.confirmAddedWorks(endpoint, putcode, logger)
+        print("%s: %s replicas added" % (thread_name, endpoint.capitalize()))        
         updatedPutcodes = random.sample(putcodes, 5)
+        print ("%s: Updating %ss - %s" % (thread_name, endpoint, updatedPutcodes))
         for putcode in updatedPutcodes:
-            self.update_activity(endpoint, putcode, xml_file)
-            print("%s: %s %s updated" % (thread_name, endpoint, putcode))
+            self.update_activity(endpoint, putcode, xml, logger)
+            logger.info("%s: %s %s updated" % (thread_name, endpoint.capitalize(), putcode))
         for putcode in updatedPutcodes:
-            self.confirmUpdatedWorks(endpoint, putcode)
-        print("%s: Confirmation - %ss updated" % (thread_name, endpoint))
-        
+            self.confirmUpdatedWorks(endpoint, putcode, logger)
+        print("%s: %s replicas updated" % (thread_name, endpoint.capitalize()))
         deletedPutcodes = random.sample(putcodes, 5)
+        print ("%s: Removing %ss - %s" % (thread_name, endpoint, deletedPutcodes))
         for putcode in deletedPutcodes:
-            self.delete_activity(endpoint, putcode)
-            print("%s: %s %s deleted" % (thread_name, endpoint, putcode))
+            self.delete_activity(endpoint, putcode, logger)
+            logger.info("%s: %s %s deleted" % (thread_name, endpoint.capitalize(), putcode))
         for putcode in deletedPutcodes:
-            self.confirmRemovedWorks(endpoint, putcode)
-        print("%s: Confirmation - %ss removed" % (thread_name, endpoint))
+            self.confirmRemovedWorks(endpoint, putcode, logger)
+        print("%s: %s replicas removed" % (thread_name, endpoint.capitalize()))
         
     def main(self):
-        t1 = threading.Thread(target=self.replicationTest, args=("Thread-1", "work", "test_work.xml") )
-        t2 = threading.Thread(target=self.replicationTest, args=("Thread-2", "education", "test_edu.xml") )
-        t3 = threading.Thread(target=self.replicationTest, args=("Thread-3", "funding", "test_funding.xml") )
-        t4 = threading.Thread(target=self.replicationTest, args=("Thread-4", "peer-review", "test_pr.xml") )
+        t1 = threading.Thread(target=self.replicationTest, args=("Thread-1", "work", "replication_test_work.xml") )
+        t2 = threading.Thread(target=self.replicationTest, args=("Thread-2", "education", "replication_test_edu.xml") )
+        t3 = threading.Thread(target=self.replicationTest, args=("Thread-3", "funding", "replication_test_funding.xml") )
+        t4 = threading.Thread(target=self.replicationTest, args=("Thread-4", "peer-review", "replication_test_pr.xml") )
         print("Cleaning record...")
         self.cleanup()
         print("Cleaning done")
@@ -164,7 +193,6 @@ class Replication(unittest.TestCase):
         t2.start()
         t3.start()
         t4.start()
-
         
 
 Replication().main()
