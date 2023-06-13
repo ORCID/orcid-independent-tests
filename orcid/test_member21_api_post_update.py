08a6fcec525fa2e75bda2d9bbef61a9edd2c8996
amontenegro <a.montenegro@orcid.org>
2023-06-13 16:03:19 -0600

    Remove tests from 2.1 and 3.0

--- orcid/test_member21_api_post_update.py
+++ orcid/test_member21_api_post_update.py
@@ -25,34 +25,13 @@ class Member20ApiPostUpdate(OrcidBaseTest.OrcidBaseTest):
             self.access, self.refresh = self.orcid_exchange_auth_token(self.client_id, self.client_secret, self.code)
 
     
-    def test_post_update_work(self):
-        # Test Post the ma test work 2
-        response = self.post_activity(self.version, "work", "ma21_work.xml")
-        self.assertTrue("HTTP/1.1 201" in response, "Response missing \"201\" code: " + response)
-        putcode = self.get_putcode_from_response(response)
-        print (putcode)
-        self.assertIsNotNone(putcode,"Not valid putcode returned: [%s]" % str(putcode))
-        # Update the work with JSON
-        self.assertFalse("" == putcode, "Empty putcode in url")
-        updated_data = '{"put-code":' + str(putcode) + ',"title":{"title":"APITestTitleUpdated21"},"type":"JOURNAL_ARTICLE","external-ids":{"external-id":[{"external-id-value":"4321","external-id-type":"doi","external-id-relationship":"SELF"}]}}'
-        update_response = self.update_activity(self.version, putcode, updated_data, "work")
-        self.assertTrue("HTTP/1.1 200" in update_response, str(putcode) + " > Update Action Response: " + update_response + " with data [%s]" % updated_data)
+    # Removed, covered here: https://github.com/ORCID/orcid-cypress_tests-private/blob/main/cypress/e2e/mapi/v2_1/crud_work_v21.cy.js
         
+    # Removed, covered here: https://github.com/ORCID/orcid-cypress_tests-private/blob/main/cypress/e2e/mapi/v2_1/crud_education_v21.cy.js
         
-    def test_post_education(self):
-        # Test Post education item to the record created today using the 2.1 api 
-        response = self.post_activity(self.version, "education", "ma21_edu.xml")
-        self.assertTrue("HTTP/1.1 201" in response, "Response missing \"201\" code: " + response)
+    # Removed, covered here: https://github.com/ORCID/orcid-cypress_tests-private/blob/main/cypress/e2e/mapi/v2_1/crud_funding_v21.cy.js
         
-    def test_post_funding(self):
-		# Test Post funding to the record created today using the 2.1 api 
-        response = self.post_activity(self.version, "funding", "ma21_fund2.xml")
-        self.assertTrue("HTTP/1.1 201" in response, "Response missing \"201\" code: " + response)
-        
-    def test_post_peerreview(self):
-		# Test Post peer review to the record created today using the 2.1 api 
-        response = self.post_activity(self.version, "peer-review", "ma21_peer2.xml")
-        self.assertTrue("HTTP/1.1 201" in response, "Response missing \"201\" code: " + response)
+    # Removed, covered here: https://github.com/ORCID/orcid-cypress_tests-private/blob/main/cypress/e2e/mapi/v2_1/crud_peerReview_v21.cy.js
         
     def test_post_update_keyword(self):
         # Test Post keyword to the record created today using the 2.1 api and test updating using put
@@ -71,20 +50,14 @@ class Member20ApiPostUpdate(OrcidBaseTest.OrcidBaseTest):
         response = self.post_activity(self.version, "other-names", "ma21_othername.xml")
         self.assertTrue("HTTP/1.1 201" in response, "Response missing \"201\" code: " + response) 
         
-    def test_post_country(self):
-		# Test Post country to the record created today using the 2.1 api
-        response = self.post_activity(self.version, "address", "ma21_country.xml")
-        self.assertTrue("HTTP/1.1 201" in response, "Response missing \"201\" code: " + response) 
+    # Removed, covered here: https://github.com/ORCID/orcid-cypress_tests-private/blob/main/cypress/e2e/mapi/v2_1/crud_address_v21.cy.js 
         
     def test_post_website(self):
 		# Test Post website to the record created today using the 2.1 api
         response = self.post_activity(self.version, "researcher-urls", "ma21_website.xml")
         self.assertTrue("HTTP/1.1 201" in response, "Response missing \"201\" code: " + response) 
         
-    def test_post_identifier(self):
-        # Test Post personal identifier to the record created today using the 2.1 api
-        response = self.post_activity(self.version, "external-identifiers", "ma21_identifier.xml")
-        self.assertTrue("HTTP/1.1 201" in response, "Response missing \"201\" code: " + response)     
+    # Removed, covered here: https://github.com/ORCID/orcid-cypress_tests-private/blob/main/cypress/e2e/mapi/v2_1/crud_externalids_v21.cy.js
 
     def test_post_update_notification(self):
 		# Test Post notification to the record created today using the 2.1 api
